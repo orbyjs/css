ee31ee4fb366bc58301a811cfd201bf983597d12
Matias Trujillo <mtrujillo@outlook.cl>
2019-01-12 15:35:57 -0300

    css is added to the export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { options } from "./options";
-import { parse } from "./parse";
 export { parse } from "./parse";
 export { options } from "./options";
-
-let ID = 0;
+import { css } from "./css";
+export { css } from "./css";
 
 export function getTagInstance(tagName) {
     let doc = options.document || document;
@@ -19,123 +18,20 @@ export function getTagInstance(tagName) {
     return options.HTMLInstances[tagName];
 }
 
-export function stateToArgs(states) {
-    return states
-        .map(
-            ({ value, args }) =>
-                value + (args.length ? `(${args.join(",")})` : "")
-        )
-        .join(":");
-}
-
-export function createRuleSelector(rule, space, parent, deep = 0) {
-    let rules = [],
-        root = "." + space,
-        selectors = rule.selectors.map(([{ value, args }, ...states]) => {
-            value = value.replace(/(\s+)$/, " ");
-            switch (value) {
-                case ":host":
-                    return args.length ? args.map(value => root + value) : root;
-                case ":global":
-                    return args.join(",");
-                case "& ":
-                case "&":
-                    return (
-                        parent +
-                        (value === "&" ? "" : " ") +
-                        stateToArgs(states)
-                    );
-                default:
-                    return deep
-                        ? (parent ? parent + " " : "") +
-                              value +
-                              stateToArgs(states)
-                        : `${root} ${value + stateToArgs(states)}`;
-            }
-        }),
-        properties = rule.properties.map(({ index, value }) => {
-            if (/^animation(-name){0,1}$/.test(index)) {
-                value = space + value;
-            }
-            return `${index}:${value}`;
-        });
-
-    if (properties.length) {
-        rules.push(`${selectors.join(",")}{${properties.join(";")}}`);
-    }
-    rule.children.map(rule => {
-        rule = selectors.map(selector =>
-            createRuleSelector(rule, space, selector, deep + 1)
-        );
-        if (rule.length) rules = rules.concat(...rule);
-    });
-
-    return rules;
-}
-
-export function createStaticCss(rules, space, parent = "") {
-    return rules.reduce((nextRules, rule) => {
-        switch (rule.type) {
-            case "keyframes":
-                return nextRules.concat(
-                    `@keyframes ${space + rule.value}{${rule.children
-                        .map(rule => createRuleSelector(rule, space, "", 1))
-                        .join("")}}`
-                );
-            case "media":
-            case "supports":
-                return nextRules.concat(
-                    `@media ${rule.value}{${rule.children
-                        .map(rule => createRuleSelector(rule, space))
-                        .join("")}}`
-                );
-            case "selector":
-                return nextRules.concat(createRuleSelector(rule, space));
-            case "import":
-                return nextRules.concat(rule.selector + ";");
-            default:
-                return nextRules;
-        }
-    }, []);
-}
-
-export function transpile(mapString, args) {
-    let customVars = {},
-        space = "--cn-" + ID++,
-        localID = 0,
-        css = mapString
-            .map((string, index) => {
-                if (typeof args[index] === "function") {
-                    let name = space + "-" + localID++;
-                    customVars[name] = args[index];
-                    string += `var(${name})`;
-                } else {
-                    string += args[index] || "";
-                }
-                return string;
-            })
-            .join("");
-    return {
-        space,
-        rules: createStaticCss(parse(css), space),
-        customVars
-    };
-}
-
 export function createStyled(pragma, enableStyleObject) {
     return function scope(tagName) {
         let isComponent = typeof tagName === "function",
             tagInstance = isComponent ? {} : getTagInstance(tagName) || {};
 
         return function styled(mapString, ...args) {
-            let { space, rules, customVars } = transpile(mapString, args),
+            let { className, rules, customVars } = css(mapString, ...args),
                 doc = options.document || document;
 
-            let existStyle = doc.getElementById(space),
+            let existStyle = doc.getElementById(className),
                 style = existStyle || doc.createElement("style");
 
             if (!existStyle) {
-                style.id = space;
+                style.id = className;
                 doc.head.appendChild(style);
             }
 
@@ -144,7 +40,7 @@ export function createStyled(pragma, enableStyleObject) {
             } else {
                 style.innerHTML = rules.join("");
             }
-            return function Component(props, context) {
+            function Component(props, context) {
                 let style = enableStyleObject
                         ? typeof props.style === "object"
                             ? props.style
@@ -153,10 +49,10 @@ export function createStyled(pragma, enableStyleObject) {
                         ? props.style
                         : "",
                     nextProps = {},
-                    className = [space];
+                    nextClassName = [className];
 
                 if (props.className || props.class) {
-                    className.push(props.className || props.class);
+                    nextClassName.push(props.nextClassName || props.class);
                 }
 
                 for (let index in customVars) {
@@ -178,12 +74,16 @@ export function createStyled(pragma, enableStyleObject) {
                     }
                 }
 
-                nextProps.className = className.join(" ");
+                nextProps.className = nextClassName.join(" ");
 
                 nextProps.style = style;
 
                 return pragma(tagName, nextProps, props.children);
-            };
+            }
+
+            Component.className = className;
+            Component.is = alias => className + alias;
+            return Component;
         };
     };
 }
